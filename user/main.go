78a9fbcab065f4b7236b5af4c9441962e86b3670
kangxiaodong <kangxiaodong@sayinfo.cn>
2020-04-28 18:29:14 +0800

    add transation mqtt

--- user/main.go
+++ user/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"user/router"
 	"user/router/v1"
 )
@@ -9,4 +10,7 @@ func main() {
 	router.Init()
 	v1.Init()
 	router.GinApp.Run(":8888")
+
+
+	http.ListenAndServe("",nil)
 }
